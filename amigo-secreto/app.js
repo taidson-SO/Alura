e6bb7e97aabb15aa3bea3de8f5375fcfbcee0952
taidson <taidson2@gmail.com>
2025-01-19 17:53:03 -0300

    Implementação da função adicionarAmigos

--- amigo-secreto/app.js
+++ amigo-secreto/app.js
@@ -1,1 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosConfirmados = [];
+let entradaAmigos = document.querySelector('input');
+
+function adicionarAmigos() 
+{
+    
+    let amigo = entradaAmigos.value;
+    
+    if (amigo == "")
+    {
+        alert("Digite o nome do amigo"); 
+    } 
+    else 
+    {
+        if(!amigosConfirmados.includes(amigo))
+        {
+            amigosConfirmados.push(amigo);
+            alert("Amigo adicionado com suscesso!!!");
+        
+        } 
+        else 
+        {
+            let opcao = prompt("Este amigo já havia confirmado, gostaria de adiciona-lo novamente? \n \
+                Digite: \n 1 - Adicionar \n 2 - Cancelar");
+                
+                if(opcao == 1)
+                {
+                    amigosConfirmados.push(amigo);
+                    alert("Amigo adicionado com suscesso");
+                } 
+                else 
+                {
+                    alert("Amigo não adicionado!");
+                }
+        }
+    } 
+    entradaAmigos.value = "";
+
+}
